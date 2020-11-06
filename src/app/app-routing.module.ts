a1968225a42c89ec76197bba99a20808e7d456da
Vimukthi Gunarathna <vimma@Vimukthis-MacBook-Air.local>
2020-11-06 15:40:16 +0530

    configuring initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './public/cart/cart.component';
+import { DashboardComponent } from './public/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo:'dashboard', pathMatch:'full' },
+  { path: 'dashboard',component: DashboardComponent },
+  { path: 'cart',component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
