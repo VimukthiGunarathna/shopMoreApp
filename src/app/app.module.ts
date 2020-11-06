9289fcfe1cfd9a7e3bffc843f19bb176427409a1
Vimukthi Gunarathna <vimma@Vimukthis-MacBook-Air.local>
2020-11-06 15:35:44 +0530

    creating component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './shared/top-nav/top-nav.component';
+import { DashboardComponent } from './public/dashboard/dashboard.component';
+import { CartComponent } from './public/cart/cart.component';
+import { ItemCardComponent } from './public/dashboard/item-card/item-card.component';
+import { PricingCardComponent } from './public/dashboard/pricing-card/pricing-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    DashboardComponent,
+    CartComponent,
+    ItemCardComponent,
+    PricingCardComponent
   ],
   imports: [
     BrowserModule,
