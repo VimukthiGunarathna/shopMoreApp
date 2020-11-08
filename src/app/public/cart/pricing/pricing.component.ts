1cdd3d8ab5f219747ee2a4c48531fa81e6064d28
VimukthiGunarathna <vimukthiappleid@gmail.com>
2020-11-08 11:56:43 +0530

    UI changes and increment ops

--- src/app/public/cart/pricing/pricing.component.ts
+++ src/app/public/cart/pricing/pricing.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingComponent implements OnInit {
 
+
+  public qty_units = 0;
+  public qty_cartons = 0;
+  public products;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  incrementOperation(type) {
+    if (type == "carton") {
+      if (this.qty_cartons >= 0) {
+        this.qty_cartons++;
+      }
+    } else {
+      if (this.qty_units >= 0) {
+        this.qty_units++;
+      }
+    }
+  }
+  decrementOperation(type) {
+    if (type == "carton") {
+      if (this.qty_cartons > 0) {
+        this.qty_cartons--;
+      }
+    } else {
+      if (this.qty_units > 0) {
+        this.qty_units--;
+      }
+    }
   }
 
 }
